8be4034ca1d284c987f304494187cb7a9c47a2c8
dragons96 <521274311@qq.com>
2024-03-15 20:21:04 +0800

    update

--- {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/__main__.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/__main__.py
@@ -4,11 +4,11 @@ from {{ cookiecutter.package_name }}.config import cfg
 
 
 @click.command()
-@click.version_option()
+@click.version_option(version="1.0.0")
 def main() -> None:
     """{{cookiecutter.friendly_name}}."""
     logger.info('运行成功, 当前项目: {}', cfg.project_name)
 
 
 if __name__ == "__main__":
-    main(prog_name="{{cookiecutter.project_name}}")  # pragma: no cover
+    main()  # pragma: no cover
