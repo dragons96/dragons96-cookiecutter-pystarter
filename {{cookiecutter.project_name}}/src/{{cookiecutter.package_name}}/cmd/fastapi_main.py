9a22a48f3d0039ac12ed970e1b3e49d9bac401c6
dragons96 <521274311@qq.com>
2024-03-23 01:14:22 +0800

    update

--- {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/cmd/fastapi_main.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/cmd/fastapi_main.py
@@ -9,18 +9,20 @@ import uvicorn
 
 @click.command()
 @click.option('--project_dir', default=None, help='项目目录, 未打包无需传该参数, 自动基于项目树检索')
+@click.option('--host', default='127.0.0.1', help='服务允许访问的ip, 若允许所有ip访问可设置0.0.0.0')
 @click.option('--port', default=8000, help='服务端口')
 @click.option('--workers', default=1, help='工作进程数')
 @click.version_option(version="1.0.0", help='查看命令版本')
 @click.help_option('-h', '--help', help='查看命令帮助')
 def main(project_dir: Optional[str] = None,
+         host: Optional[str] = '127.0.0.1',
          port: Optional[int] = 8000,
          workers: Optional[int] = 1) -> None:
     """{{cookiecutter.friendly_name}} FastAPI cmd."""
     if project_dir:
         os.environ['PROJECT_DIR'] = project_dir
     logger.info('运行成功, 当前项目: {}', cfg().project_name)
-    uvicorn.run('{{cookiecutter.package_name}}.fastapi.app:app', host='0.0.0.0', port=port, workers=workers)
+    uvicorn.run('{{cookiecutter.package_name}}.fastapi.app:app', host=host, port=port, workers=workers)
 
 
 if __name__ == "__main__":
