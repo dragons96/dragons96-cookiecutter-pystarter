689c02ad70a1cbd2dac77010943ee0f8e51d162c
dragons96 <521274311@qq.com>
2024-04-12 11:36:00 +0800

    新增默认执行脚本

---
+++ {{cookiecutter.project_name}}/bin/fastapi_main.sh
@@ -0,0 +1,10 @@
+# 获取脚本文件目录
+BIN_DIR=$(dirname "$(readlink -f "$0")")
+# 获取项目目录
+PROJECT_DIR=$(dirname "$BIN_DIR")
+echo "当前项目路径: $PROJECT_DIR"
+# 进入目录
+cd "$PROJECT_DIR"
+echo "切换到项目目录: $PROJECT_DIR"
+# 执行命令 --workers 工作进程数, 正式环境可根据CPU核数设置
+nohup poetry run fastapi_main --host 0.0.0.0 --port 8000 --env dev --workers 1 >> /dev/null 2>&1 &
