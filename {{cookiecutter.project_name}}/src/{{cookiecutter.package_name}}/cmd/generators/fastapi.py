0da7a49b03b297069c5a81bf664908c5e9e08fd7
dragons96 <521274311@qq.com>
2024-04-24 19:12:32 +0800

    update

--- {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/cmd/generators/fastapi.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/cmd/generators/fastapi.py
@@ -89,5 +89,31 @@ if __name__ == "__main__":
 ''', override=override)
         add_poetry_script(project_dir, 'fastapi_main = "{{cookiecutter.package_name}}.cmd.fastapi_main:main"')
 
+        bin_dir = project_dir + os.sep + 'bin'
+        bin_file = bin_dir + os.sep + 'fastapi_main.sh'
+        create_file(bin_file, '''# 获取脚本文件目录
+BIN_DIR=$(dirname "$(readlink -f "$0")")
+# 获取项目目录
+PROJECT_DIR=$(dirname "$BIN_DIR")
+echo "当前项目路径: $PROJECT_DIR"
+# 进入目录
+cd "$PROJECT_DIR"
+echo "切换到项目目录: $PROJECT_DIR"
+echo "拉取最新项目代码"
+git pull
+echo "切换虚拟环境"
+source venv/bin/activate
+echo "开始安装生产环境依赖"
+poetry install --only main
+echo "安装生产环境依赖完成"
+# 执行命令
+echo "开始执行命令"
+# 执行命令 --workers 工作进程数, 正式环境可根据CPU核数设置
+nohup poetry run fastapi_main --host 0.0.0.0 --port 8000 --env dev --workers 4 >> /dev/null 2>&1 &
+echo "执行成功"
+echo "退出虚拟环境"
+deactivate
+''')
+
     gen_fastapi_dir()
     gen_fastapi_cmd()
