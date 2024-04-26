23a62ae37a672fed8bb6bf2c2c72c53d139ba581
dragons96 <521274311@qq.com>
2024-04-26 16:57:12 +0800

    update

--- {{cookiecutter.project_name}}/tests/test_main.py
+++ {{cookiecutter.project_name}}/tests/test_main.py
@@ -2,7 +2,7 @@
 import pytest
 from click.testing import CliRunner
 
-from {{cookiecutter.package_name}}.cmd.main import __main__
+from {{cookiecutter.package_name}}.cmd.main import main
 
 
 @pytest.fixture
@@ -13,5 +13,5 @@ def runner() -> CliRunner:
 
 def test_main_succeeds(runner: CliRunner) -> None:
     """It exits with a status code of zero."""
-    result = runner.invoke(__main__.main)
+    result = runner.invoke(main)
     assert result.exit_code == 0
