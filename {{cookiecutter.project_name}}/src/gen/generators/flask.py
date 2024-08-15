ae3d8c69b55c310570ddfd7dd75958686216c3c6
dragons96 <jinqunlong@black-unique.com>
2024-08-15 11:47:49 +0800

    update

--- {{cookiecutter.project_name}}/src/gen/generators/flask.py
+++ {{cookiecutter.project_name}}/src/gen/generators/flask.py
@@ -110,6 +110,21 @@ echo "开始安装生产环境依赖"
 poetry install --only main
 echo "安装生产环境依赖完成"
 echo "开始检查是否已存在运行中的进程"
+pids=$(pgrep -f '{{cookiecutter.project_name}}/venv/bin/flask')
+if [ -z "$pids" ]; then
+  echo "无正在运行中的进程, 忽略"
+else
+  # 通过for循环遍历所有进程ID
+  for pid in $pids; do
+    echo "存在正在运行中的进程: $pid, 即将终止进程..."
+    kill "$pid" # 使用引号以确保ID作为参数正确传递
+    if [ $? -eq 0 ]; then
+      echo "进程: $pid 已被成功终止"
+    else
+      echo "进程: $pid 终止失败"
+    fi
+  done
+fi
 pids=$(pgrep -f '{{cookiecutter.project_name}}-[A-Za-z0-9_]*-py3.[0-9]+/bin/flask')
 if [ -z "$pids" ]; then
   echo "无正在运行中的进程, 忽略"
